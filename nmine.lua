833d97df53410174ed298204e43ab9774ea9b424
Oleksandr Dievri <o.dievri@gmail.com>
2020-09-25 13:47:20 +0300

    add shuffled empty nodes

--- nmine.lua
+++ nmine.lua
@@ -9,3 +9,11 @@ function nmine.populate_inventory(inventory, ...)
 end
 
 
+function nmine.shuffle(content)
+    local shuffled = {}
+    for i, v in ipairs(content) do
+        local pos = math.random(1, #shuffled + 1)
+        table.insert(shuffled, pos, v)
+    end
+    return shuffled
+end
--- platforms.lua
+++ platforms.lua
@@ -23,7 +23,8 @@ function platforms.wipe(origin, size, orientation)
 end
 
 function platforms.set_meta(origin, size, orientation, meta_name, meta_data)
-    local meta = minetest.serialize(meta_data)
+    local empty_nodes = {}
+    local meta = meta_data == nil and nil or minetest.serialize(meta_data)
     local x_end = origin.x + size
     local y_end = orientation == "horizontal" and origin.y or origin.y + size
     local z_end = orientation == "horizontal" and origin.z + size or origin.z
@@ -31,16 +32,22 @@ function platforms.set_meta(origin, size, orientation, meta_name, meta_data)
     for z = origin.z, z_end do
         for y = origin.y, y_end do
             for x = origin.x, x_end do
-                local node = minetest.get_meta({x = x, y = y, z = z})
+                local point = {x = x, y = y, z = z}
+                local node = minetest.get_meta(point)
                 node:set_string(meta_name, meta)
+                table.insert(empty_nodes, point)
             end
         end
     end
+
+    empty_nodes = nmine.shuffle(empty_nodes)
+
+    platforms.set_meta_origin(origin, "empty_nodes", empty_nodes)
 end
 
 function platforms.set_meta_origin(origin, meta_name, meta_data)
     local meta = minetest.serialize(meta_data)
-    local node = minetest.get_meta({x = x, y = y, z = z})
+    local node = minetest.get_meta({x = origin.x, y = origin.y, z = origin.z})
     node:set_string(meta_name, meta)
 end
 
