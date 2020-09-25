ea81931f8fc001eb040478cb846941e6834ac579
Oleksandr Dievri <o.dievri@gmail.com>
2020-09-25 11:11:37 +0300

    set metadata on a whole platform

--- platforms.lua
+++ platforms.lua
@@ -11,3 +11,30 @@ function platforms.create(origin, size, orientation, node_name)
     end
     worldedit.set(pos1, pos2, node_name)
 end
+
+function platforms.set_meta(origin, size, orientation, meta_name, meta_data)
+    local meta = minetest.serialize(meta_data)
+    local pos1 = origin
+    local x_st = pos1.x
+    local y_st = pos1.y
+    local z_st = pos1.z
+
+    local x_end = pos1.x + size
+    local y_end = orientation == "horizontal" and pos1.y or pos1.y + size
+    local z_end = orientation == "horizontal" and pos1.z + size or pos1.z
+
+    for z = z_st, z_end do
+        for y = y_st, y_end do
+            for x = x_st, x_end do
+                local node = minetest.get_meta({x = x, y = y, z = z})
+                node:set_string(meta_name, meta)
+            end
+        end
+    end
+end
+
+function platforms.set_meta_origin(origin, meta_name, meta_data)
+    local meta = minetest.serialize(meta_data)
+    local node = minetest.get_meta({x = x, y = y, z = z})
+    node:set_string(meta_name, meta)
+end
